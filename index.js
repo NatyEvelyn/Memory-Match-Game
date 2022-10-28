4cd86e9629c96de47d0d350aa16d7043667d6784
natyevelyn <evelynat.x@gmail.com>
2022-10-28 01:03:25 -0300

    Finalização do Jogo

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const input = document.querySelector(".login-input");
 const button = document.querySelector(".login-button");
 const form = document.querySelector(".login-form");
-//function validateInput(){}
-//console.log(event.target.value);
-/*através do evento, conseguimos recuperar o target, que seria o input.
-Depois de pegar o input com target, usamos value para pegar o valor,
-que seria o que está sendo digitado.*/
-/*Acessamos event e target, mas dá para fazer direto
-através da object destructure = desestruturação de objetos */
 
 const validateInput = ({ target }) => {
   if (target.value.length > 1) {
